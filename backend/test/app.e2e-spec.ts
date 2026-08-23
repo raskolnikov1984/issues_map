741413120cd857037d33ddc082e8abd18ea6e4bc
aserrador <alejandro.ayala@onecluster.org>
2026-08-22 20:24:29 -0500

    fix(backend): use dynamic app module registration in e2e test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -9,7 +9,7 @@ describe('HealthController (e2e)', () => {
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule.register()],
     }).compile();
 
     app = moduleFixture.createNestApplication();
